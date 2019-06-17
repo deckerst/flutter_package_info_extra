72890a8ab7d0e5499881aafb06a96e09a768d695
Thibault Deckers <thibault.deckers@gmail.com>
2019-06-17 13:57:39 +0900

    restored iOS part from default plugin template

---
+++ ios/Classes/FlutterPackageInfoExtraPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterPackageInfoExtraPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterPackageInfoExtraPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterPackageInfoExtraPlugin.h"
+
+@implementation FlutterPackageInfoExtraPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_package_info_extra"
+            binaryMessenger:[registrar messenger]];
+  FlutterPackageInfoExtraPlugin* instance = [[FlutterPackageInfoExtraPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
